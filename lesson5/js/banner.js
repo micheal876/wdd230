8bfcb68a6a3b29315ce1fe0ac8dc326aaf64bbcc
Micheal Edwards <77127122+micheal876@users.noreply.github.com>
2021-10-24 04:13:38 -0500

    init

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,4 +1,4 @@
-if (d.getDay() == [0]) {
+if (d.getDay() == [5]) {
     document.getElementById("banner").innerHTML = ("Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.");
 
 } else {
