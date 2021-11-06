88247e04b4465c255fb3ff26b5085b79276aed7a
Micheal Edwards <77127122+micheal876@users.noreply.github.com>
2021-11-06 12:54:56 -0500

    init

--- lesson7-1/js/lazyload.js
+++ lesson7-1/js/lazyload.js
@@ -1,38 +1,30 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+// Get images with data src attribute
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 
-/*function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-    img.src = src;
-}*/
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 50px 0px"
+    threshold: 2,
+    rootMargin: "0px 0px 300px 0px"
 };
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
-    };
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
-if ("IntersectionObserver" in window) {
-    const observer = new IntersectionObserver((items, imgObserver) => {
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
-            if (item.isIntersecting) {
+            if(item.isIntersecting) {
                 loadImages(item.target);
-                imgObserver.unobserve(item.target);
+                observer.unobserve(item.target);
             }
         });
-
-    }, imgOptions);
-
-
+    });
     imagesToLoad.forEach((img) => {
         observer.observe(img);
-    });
+    }, imgOptions);
 } else {
     imagesToLoad.forEach((img) => {
         loadImages(img);
