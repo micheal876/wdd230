50d1cd7ddb2f1a549f145cac655d279fc97ff50c
Micheal Edwards <77127122+micheal876@users.noreply.github.com>
2021-11-29 12:03:16 -0500

    init

--- lesson10/js/prestonweatherapi.js
+++ lesson10/js/prestonweatherapi.js
@@ -1,29 +1,69 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=7f0d84b192d7b06436b826401d169039";
+function windChill(tempF, windSpeed) {
+  return 35.74 +
+    0.6215 * tempF -
+    35.75 * Math.pow(windSpeed, 0.16) +
+    0.4275 * tempF * Math.pow(windSpeed, 0.16);
+}
+
+// Information for current weather
+const apiURLcurrent =
+"https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
+fetch(apiURLcurrent)
+.then((response) => response.json())
+.then((jsObject) => {
+  document.querySelector("#weathertype").textContent =
+    jsObject.weather[0].description;
+  document.querySelector("#tempF").textContent =
+    jsObject.main.temp.toFixed(0);
+  document.querySelector("#humidity").textContent = jsObject.main.humidity;
+  document.querySelector("#windspeed").textContent = jsObject.wind.speed;
+
+  // if parameters are met, wind chill will be called
+  if (jsObject.main.temp <= 50 && jsObject.wind.speed >= 3) {
+      chill = windChill(jsObject.main.temp, jsObject.wind.speed);
+      document.querySelector("#windchill").textContent = chill;
+  }
+  else {
+      document.querySelector("#windchill").textContent = 'N/A';
+  };
+});
+
+// Information for forecast
+const apiURL =
+"https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=9cef91c93e68d2c9661f7465a0530962";
 
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    document.getElementById('high-temp').textContent = jsObject.main.temp_max;
-    document.getElementById('humidity').textContent = jsObject.main.humidity;
-    document.getElementById('speed').textContent = jsObject.wind.speed;
+  let day = 1;
+  const dayofWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
 
+  const fiveDayForecast = jsObject.list.filter((forecast) =>
+    forecast.dt_txt.includes("18:00:00")
+  );
 
-    
-    var temp = parseFloat(document.getElementById("current-temp").innerHTML);
-    var speed = parseFloat(document.getElementById("speed").innerHTML);
+  fiveDayForecast.forEach((x) => {
+    let d = new Date(x.dt_txt);
+    document.querySelector(`#dayofweek${day}`).textContent =
+      dayofWeek[d.getDay()];
+    document.querySelector(`#dailytemp${day}`).textContent =
+      x.main.temp.toFixed(0);
 
-    var f = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(speed,0.16);
- console.log(f)
-      if (temp <= 50 && speed > 3) {
-        document.getElementById("windchill").innerHTML = f.toFixed(2);
-      }  
-else {
-    document.getElementById("windchill").innerHTML = "N/A";
-}
-    
-    
-    
+    let iconimg = document.createElement("img");
 
+    const imagesrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+    const desc = x.weather[0].description;
+
+    iconimg.setAttribute("src", imagesrc);
+    iconimg.setAttribute("alt", desc);
+
+    document.querySelector(`#icon${day}`).appendChild(iconimg);
+    //document.querySelector(`#icon${day}`).setAttribute("src", imagesrc);
+    //document.querySelector(`#icon${day}`).setAttribute("alt", desc);
+
+    day++;
+  });
+
+  /*
+   */
 });
